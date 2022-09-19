f6fc21533bca4ca140da5d54ff02f2e8426c5c5f
ArthurVeraci <arthurveraci5@gmail.com>
2022-09-18 21:38:36 -0300

    Adicionar arquivos de projeto.

---
+++ ATIVIDADE2/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ATIVIDADE2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtCamisa = new System.Windows.Forms.TextBox();
+            this.Npesquisar = new System.Windows.Forms.Button();
+            this.Ngravar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(223, 77);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(232, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(223, 120);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(60, 20);
+            this.txtAltura.TabIndex = 1;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(223, 163);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(60, 20);
+            this.txtPeso.TabIndex = 2;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(223, 206);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(60, 20);
+            this.txtIdade.TabIndex = 3;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(223, 250);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(209, 20);
+            this.txtTime.TabIndex = 4;
+            // 
+            // txtCamisa
+            // 
+            this.txtCamisa.Location = new System.Drawing.Point(223, 296);
+            this.txtCamisa.Name = "txtCamisa";
+            this.txtCamisa.Size = new System.Drawing.Size(60, 20);
+            this.txtCamisa.TabIndex = 5;
+            // 
+            // Npesquisar
+            // 
+            this.Npesquisar.Location = new System.Drawing.Point(119, 349);
+            this.Npesquisar.Name = "Npesquisar";
+            this.Npesquisar.Size = new System.Drawing.Size(104, 48);
+            this.Npesquisar.TabIndex = 6;
+            this.Npesquisar.Text = "PESQUISAR";
+            this.Npesquisar.UseVisualStyleBackColor = true;
+            this.Npesquisar.Click += new System.EventHandler(this.Npesquisar_Click);
+            // 
+            // Ngravar
+            // 
+            this.Ngravar.Location = new System.Drawing.Point(452, 349);
+            this.Ngravar.Name = "Ngravar";
+            this.Ngravar.Size = new System.Drawing.Size(104, 48);
+            this.Ngravar.TabIndex = 7;
+            this.Ngravar.Text = "GRAVAR";
+            this.Ngravar.UseVisualStyleBackColor = true;
+            this.Ngravar.Click += new System.EventHandler(this.Ngravar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleGreen;
+            this.label1.Location = new System.Drawing.Point(172, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.PaleGreen;
+            this.label2.Location = new System.Drawing.Point(173, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Altura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.PaleGreen;
+            this.label3.Location = new System.Drawing.Point(176, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Peso";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.PaleGreen;
+            this.label4.Location = new System.Drawing.Point(173, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Idade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.PaleGreen;
+            this.label5.Location = new System.Drawing.Point(177, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Time";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.PaleGreen;
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(151, 299);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "N° Camisa";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ATIVIDADE2.Properties.Resources.istockphoto_867727588_1024x1024;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ngravar);
+            this.Controls.Add(this.Npesquisar);
+            this.Controls.Add(this.txtCamisa);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtCamisa;
+        private System.Windows.Forms.Button Npesquisar;
+        private System.Windows.Forms.Button Ngravar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
